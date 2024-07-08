40e588e71930ed62caa33749b2f676e508f2b9b8
Johans Eris <johans.justs.eris@gmail.com>
2024-07-08 22:25:10 +0300

    Add semi working template

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Categories of products.
+    """
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Customer(models.Model): 
+    """
+    Customers that can purchase Products.
+    """
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    
+class Product(models.Model):
+    """
+    Products that can be purchases by the Customer.
+    """
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6) # 9999.99
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=200, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/') # Goes to media/uploads/product # Need pillow dep. for this.
+    
+    def __str__(self):
+        return f'{self.name} {self.price}'
+    
+    
+class Order(models.Model):
+    """
+    Orders made for Products by the Customers.
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.product} {self.customer} {self.quantity}'
+    
